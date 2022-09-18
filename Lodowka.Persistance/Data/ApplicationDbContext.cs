f2d0eceeba86fb694375d655fb167f766c6a2e0e
Daniel Perwejnis <daniel.perwejnis@gmail.com>
2022-09-18 22:18:29 +0200

    FIx incorrect User registration in ApplicationDbContext

--- Lodowka.Persistance/Data/ApplicationDbContext.cs
+++ Lodowka.Persistance/Data/ApplicationDbContext.cs
@@ -1,15 +1,17 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Lodowka.Core.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace Lodowka.Persistance.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<User>
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
         public DbSet<Fridge> Fridges { get; set; }
         public DbSet<Product> AvailableProducts { get; set; }
         public DbSet<Recipe> Recipe { get; set; }
